c7aedbeaa7f57ca38d75ff06c442397b5950b95d
SJ01-max <heo8359@naver.com>
2026-05-24 18:22:06 +0900

    Initial Commit

---
+++ SpendMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpendMap
+//
+//  Created by sungjae Heo on 5/24/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
